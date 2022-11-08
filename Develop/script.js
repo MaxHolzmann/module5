824f1a9072235102d0c008c92fce5e88dba5088c
Maximilian Holzmann <max@holzmann.io>
2022-11-08 15:09:52 -0600

    finishes JS of project

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,51 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+let hourNine = document.getElementById('hour-9'); 
+let hourTen = document.getElementById('hour-10'); 
+let hourEleven = document.getElementById('hour-11'); 
+let hourTwelve = document.getElementById('hour-12'); 
+let hourOne = document.getElementById('hour-13');
+let hourTwo = document.getElementById('hour-14');
+let hourThree = document.getElementById('hour-15'); 
+let hourFour = document.getElementById('hour-16');   
+let hourFive = document.getElementById('hour-17'); 
+
+let hours = [hourNine, hourTen, hourEleven, hourTwelve, hourOne, hourTwo, hourThree, hourFour, hourFive];
+
+$(document).ready(() => {
+
+  dayjs.extend(window.dayjs_plugin_advancedFormat);
+
+
+  for(let i = 9; i < hours.length + 9; i++) {
+
+    console.log(i - 9);
+
+    if(localStorage.getItem(i - 9) !== null) {
+      console.log("item " + (i - 9) + " found")
+      hours[i - 9].children[1].textContent = localStorage.getItem(i - 9);
+    }
+
+    if(i == dayjs().hour()) {
+      hours[i - 9].classList.add("present");
+      hours[i - 9].classList.remove('past', 'future')
+    }
+    if(i < dayjs().hour()){
+      hours[i - 9].classList.add('past');
+      hours[i - 9].classList.remove('present', 'future');
+    }
+    if(i > dayjs().hour()){
+      hours[i - 9].classList.add('future');
+      hours[i - 9].classList.remove('present', 'past');
+    }
+
+  }
+
+  $("button").click((e) => {
+    let targetButton = e.target.value;
+    let sibling = e.target.previousElementSibling;
+    localStorage.setItem(+targetButton, sibling.value.toString());
+  })  
+  
+
+    $("#currentDay").text(dayjs().format('dddd' + ', ' + 'MMMM' + " Do" + ", h:mma" + ""));
 });
+
